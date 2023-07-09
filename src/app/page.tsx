692200136f50e35e43833df172daedfe7fc48b85
unknown <ncoop720@yahoo.com>
2023-07-09 18:52:40 -0400

    phaser init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,44 @@
+"use client"
+
+import { useEffect, useState } from "react"
 
 const Home = () => {
-  return <p>Home Page</p>
+  const [game, setGame] = useState<Phaser.Game>()
+
+  useEffect(() => {
+    const initPhaser = async () => {
+      const Phaser = await import("phaser")
+
+      const create = () => {
+
+      }
+      
+      const preload = () => {
+      
+      }
+      
+      const update = () => {
+      
+      }
+
+      const phaserGame = new Phaser.Game({
+        type: Phaser.AUTO,
+        parent: 'phaser-game',
+        width: 800,
+        height: 600,
+        scene: {
+          create,
+          preload,
+          update
+        }
+      })
+
+      setGame(phaserGame)
+    }
+
+    initPhaser()
+  }, [])
+  return <div id="phaser-game" key="phaser-game" />
 }
 
 export default Home
